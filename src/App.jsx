aa07f1f279888263156783bc44321276170b790b
iepmas <sampei7511@gmail.com>
2024-06-21 17:25:08 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoadiing] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(!showModal);
+  }
+
+  useEffect(() => {
+    async function get() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_KEY}`;
+
+      const today = new Date().toDateString();
+      const localKey = `NASA-${today}`;
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey));
+        setData(apiData);
+        console.log("Fetched from cache today")
+        return
+      }
+      
+      localStorage.clear();
+
+      try {
+        const response = await fetch(url);
+        const apiData = await response.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData))
+        setData(apiData);
+        console.log("Fetched from NASA API today")
+        console.log("DATA\n", apiData);
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+    get();
+  }, []);
+
+  return (
+    <>
+      {data ? (
+        <Main data={data} />
+      ) : (
+        <div className="loadingState">
+          <i className="fa-solid fa-spinner"></i>
+        </div>
+      )}
+      {showModal && (
+        <Sidebar data={data} handleToggleModal={handleToggleModal} />
+      )}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
+    </>
+  );
+}
+
+export default App;
